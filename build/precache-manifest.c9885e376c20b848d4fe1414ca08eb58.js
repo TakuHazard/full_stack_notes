00ee14ff0e60ab90b015cc305a80e677ceae8563
TakuHazard <marwendot@gmail.com>
2020-06-09 19:41:09 -0700

    Added folder

---
+++ build/precache-manifest.c9885e376c20b848d4fe1414ca08eb58.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41657527520a82d892e721d9adcde9c5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4d63ef9733cbf0e83765",
+    "url": "/static/css/main.56bceb35.chunk.css"
+  },
+  {
+    "revision": "f7f5fa5acd796c5f0495",
+    "url": "/static/js/2.20ba4b97.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.20ba4b97.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4d63ef9733cbf0e83765",
+    "url": "/static/js/main.f23fa3f4.chunk.js"
+  },
+  {
+    "revision": "aa64dd1b2f38c7544761",
+    "url": "/static/js/runtime-main.ac8f8f6f.js"
+  }
+]);
