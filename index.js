4d79142171b7931f8f574a52e0ec8b45ad87a8e7
TakuHazard <marwendot@gmail.com>
2020-06-19 21:52:46 -0700

    Ready for deployment

--- index.js
+++ index.js
@@ -1,35 +1,26 @@
+if (process.env.NODE_ENV !== 'production') {
+  require('dotenv').config()
+}
 const express = require('express')
+const bodyParser = require('body-parser') 
 const app = express()
-require('dotenv').config()
 const Note = require('./models/note')
 
-const cors = require('cors')
+const logger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
 
-app.use(cors())
-app.use(express.json())
 app.use(express.static('build'))
+app.use(bodyParser.json())
+app.use(logger)
 
 app.get('/api/notes', (request, response) => {
   Note.find({}).then(notes => {
     response.json(notes.map(note => note.toJSON()))
-  })
-})
-
-app.post('/api/notes', (request, response) => {
-  const body = request.body
-
-  if (body.content === undefined) {
-    return response.status(400).json({ error: 'content missing' })
-  }
-
-  const note = new Note({
-    content: body.content,
-    important: body.important || false,
-    date: new Date(),
-  })
-
-  note.save().then(savedNote => {
-    response.json(savedNote.toJSON())
   })
 })
 
@@ -41,6 +32,23 @@ app.get('/api/notes/:id', (request, response, next) => {
       } else {
         response.status(404).end()
       }
+    })
+    .catch(error => next(error))
+})
+
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
+
+  const note = new Note({
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+  })
+
+  note.save()
+    .then(savedNote => savedNote.toJSON)
+    .then(savedAndFormattedNote =>{
+      response.json(savedAndFormattedNote)
     })
     .catch(error => next(error))
 })
@@ -79,15 +87,16 @@ const errorHandler = (error, request, response, next) => {
 
   if (error.name === 'CastError' && error.kind == 'ObjectId') {
     return response.status(400).send({ error: 'malformatted id' })
-  } 
+  }  else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
   next(error)
 }
 
 app.use(errorHandler)
 
-const PORT = process.env.PORT 
-
+const PORT = process.env.PORT
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
