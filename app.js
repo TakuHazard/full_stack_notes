aec3d9bc784adb0c90c6f3cc7575443999824d87
TakuHazard <marwendot@gmail.com>
2020-07-06 19:38:41 -0700

    Added authorization tokens

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ const notesRouter = require('./controllers/notes')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 const mongoose = require('mongoose')
+const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 require('express-async-errors')
 
 logger.info('connecting to', config.MONGODB_URI)
@@ -23,6 +25,8 @@ app.use(express.json())
 app.use(middleware.requestLogger)
 
 app.use('/api/notes',notesRouter)
+app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
--- controllers/notes.js
+++ controllers/notes.js
@@ -1,8 +1,12 @@
 const notesRouter = require('express').Router()
 const Note = require('../models/note')
+const User = require('../models/user')
+const jwt = require('jsonwebtoken')
 
 notesRouter.get('/', async (request, response) => {
-    const notes = await Note.find({})
+    const notes = await Note
+        .find({})
+        .populate('user',{ username : 1, name : 1 })
     response.json(notes.map(note => note.toJSON()))
 })
 
@@ -19,22 +23,38 @@ notesRouter.get('/:id', async (request, response, next) => {
     }
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+        return authorization.substring(7)
+    }
+    return null
+}
 
-notesRouter.post('/', async (request, response, next) => {
+notesRouter.post('/', async (request, response) => {
     const body = request.body
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id){
+        return response.status(401).json({ error : 'token missing or invalid ' })
+    }
 
+    console.log('decodedToken is ', decodedToken)
+    const user = await User.findById(decodedToken.id)
+
+    console.log('user is ', user)
     const note = new Note({
         content: body.content,
         important: body.important === undefined ? false : body.important,
         date: new Date(),
+        user: user._id
     })
 
-    try {
-        const savedNote = await note.save()
-        response.json(savedNote.toJSON())
-    } catch(exception) {
-        next(exception)
-    }
+    const savedNote = await note.save()
+    user.notes = user.notes.concat(savedNote._id)
+    await user.save()
+
+    response.json(savedNote.toJSON())
 })
 
 notesRouter.put('/:id', (request, response, next) => {
@@ -51,7 +71,7 @@ notesRouter.put('/:id', (request, response, next) => {
         .catch(error => next(error))
 })
 
-notesRouter.delete('/:id', async (request, response, next) => {
+notesRouter.delete('/:id', async (request, response) => {
     await Note.findByIdAndRemove(request.params.id)
     response.status(204).end()
 })
